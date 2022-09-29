4315cad7955ac793a8b2ab061e1a8dbf8532addd
Raphaelsants <111981321+Raphaelsants@users.noreply.github.com>
2022-09-29 10:19:29 -0300

    Add files via upload

---
+++ 28_09_2022/Atividade1.js
@@ -0,0 +1,16 @@
+/*
+Defina o que seria uma linguagem de programação e descreva as principais características?
+É uma linguagem formal que, através de uma série de instruções, permite que um programador escreva um conjunto de ordens, ações consecutivas e algoritmos.
+
+Descreva as principais características da linguagem javaScript.
+Voltada para HTML e CSS.
+
+Quais ambientes o javaScript pode ser executado?
+Navegador web.
+
+Quais são os motivos da popularidade do javaScript na comunidade de desenvolvimento?
+Mercado de trabalho, versatilidade e constante evolução.
+
+Por que persistência e atenção aos detalhes são habilidades e mentalidades importantes para um desenvolvedor JavaScript?
+Persistência para você se manter desmotivado e sempre ter uma crescente evolução dentro do mercado de trabalho e atenção aos detalhes para sempre estar atento no que você está fazendo, para evitar que futuros erros aconteçam.
+*/
